e83d0f2876e823344cb4da1f1094089557208558
light <balathan2004@gmail.com>
2024-10-14 20:21:26 +0530

    authentication fixed but no test

--- components/server/get_meta_doc.ts
+++ components/server/get_meta_doc.ts
@@ -1,34 +1,27 @@
 import { firestore_admin } from "@/config/firebase_admin";
-import { metadata,userInterface } from "../interfaces/shared";
+import { metadata, userInterface } from "../interfaces/shared";
+import { dummyUser } from "../interfaces/shared";
 
-
-
-export default async function GetMetaDoc(userDoc:userInterface):Promise<metadata>{
-
-  const {uid,account_type}=userDoc
+export default async function GetMetaDoc(
+  userDoc: userInterface
+): Promise<metadata> {
+  const { uid, account_type } = userDoc;
 
   if (account_type === "professional") {
-    
+    var meta_doc =
+      ((
+        await firestore_admin
+          .collection("/professional_account_meta")
+          .doc(uid)
+          .get()
+      ).data() as metadata) || dummyUser.meta_data;
 
-    var meta_doc = (
-      await firestore_admin
-        .collection("/professional_account_meta")
-        .doc(uid)
-        .get()
-    ).data() as metadata;
-
-    return meta_doc  ;
+    return meta_doc;
   } else {
-
-
     var meta_doc = (
-      await firestore_admin
-        .collection("/personal_account_meta")
-        .doc(uid)
-        .get()
+      await firestore_admin.collection("/personal_account_meta").doc(uid).get()
     ).data() as metadata;
 
-    return meta_doc 
+    return meta_doc;
   }
-
 }
